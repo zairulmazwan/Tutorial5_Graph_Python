611e9291f69cc9088b11628b4258a200f179a668
zairulmazwan <zairulmazwan@gmail.com>
2024-07-09 12:18:41 +0100

    9 Jul 2024, 12.18

--- Task2/main.py
+++ Task2/main.py
@@ -6,6 +6,8 @@ my_graph.create_city_graph()
 
 print("Is Kuala Lumpur in the graph? ",my_graph.contain_city("Kuala Lumpur"))
 print("Is London in the graph? ", my_graph.contain_city("London"))
+print("Is Qatar in the graph? ", my_graph.contain_city("Qatar"))
+
 
 print("Where can I travel from Kuala Lumpur? ")
 my_graph.print_edges(my_graph.get_city_vertex("Kuala Lumpur"))
